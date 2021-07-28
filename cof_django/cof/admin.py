277cf19f45f078405182f77e7dde6bac906a9599
kortmiranda <kortneymiranda@outlook.com>
2021-07-28 11:18:35 -0500

    configure admin

--- cof_django/cof/admin.py
+++ cof_django/cof/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # from rest_framework.relations import HyperlinkedRelatedField
 from .models import Product
 
+admin.site.register(Product)
+
 # class ProductSerializers(serializers.HyperlinkedModelSerializer):
 #     class Meta:
 #         model = Product
